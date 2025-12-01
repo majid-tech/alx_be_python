1e654e84cf5153fcb452bede921789ee63e0b13c
majid-tech <majidmohammed044@gmail.com>
2025-12-01 16:06:38 +0000

    create bank account

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -7,11 +7,12 @@ class BankAccount:
         self.account_balance += amount
     
     def withdraw(self, amount):
-        if self.account_balance > amount:
+        if self.account_balance >= amount:
             self.account_balance -= amount
+            return True
         else:
-            return f'account balance is less than amount'
+            return False
 
     def display_balance(self):
-        print(f'Current Balance: {self.account_balance}')
+        print(f'Current Balance: ${self.account_balance}')
 

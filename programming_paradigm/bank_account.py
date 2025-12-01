da3c2c37c0cbe4cb06d1da2b7ac3f72c1585df76
majid-tech <majidmohammed044@gmail.com>
2025-12-01 15:59:37 +0000

    create bank account

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -13,5 +13,5 @@ class BankAccount:
             return f'account balance is less than amount'
 
     def display_balance(self):
-        print(f'Account balance: {self.account_balance}')
+        print(f'Current Balance: {self.account_balance}')
 

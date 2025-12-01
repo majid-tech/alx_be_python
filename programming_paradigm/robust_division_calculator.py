adb1db2f92f05330ec2b00bbe24ad34a8417bdcb
majid-tech <majidmohammed044@gmail.com>
2025-12-01 16:29:53 +0000

    implement error handling

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -2,7 +2,7 @@
 
 def safe_divide(numerator, denominator):
     try:
-        return float(numerator) / float(denominator)
+        return f'The result of the division is {float(numerator) / float(denominator)}'
     except ZeroDivisionError:
         return f'Error: Cannot divide by zero.'
     except ValueError:

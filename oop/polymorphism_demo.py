a099fce56f030217552d7544ed0040485ee00850
majid-tech <majidmohammed044@gmail.com>
2025-12-11 12:39:48 +0000

    implement polymorphism and method overriding

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,26 @@
+import math
+
+class Shape:
+    def __init__(self):
+        pass
+        
+    def area(self):
+        raise NotImplementedError("The subclasses didn't implement method class.")
+    
+class Rectangle(Shape):
+    def __init__(self, length: float, width: float):
+        super().__init__()
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+    
+class Circle(Shape):
+    def __init__(self, radius: float):
+        super().__init__()
+        self.radius = radius
+
+    def area(self):
+        return math.pi * (self.radius * self.radius)
+    

4e7ea7c3d7dc8e6a7660a5f0da0518019c1faa5b
majid-tech <majidmohammed044@gmail.com>
2025-12-11 12:08:51 +0000

    include str methods to subclasses

--- oop/library_system.py
+++ oop/library_system.py
@@ -3,7 +3,7 @@ class Book:
         self.title = title
         self.author = author
     def __str__(self):
-        return f'{self.title} by {self.author}'
+        return f'Book: {self.title} by {self.author}'
     
 class EBook(Book):
     def __init__(self, title, author, file_size: int):
@@ -11,14 +11,14 @@ class EBook(Book):
         super().__init__(title, author)
 
     def __str__(self):
-        return f'{self.title} by {self.author}, File Size: {self.file_size}KB'
+        return f'EBook: {self.title} by {self.author}, File Size: {self.file_size}KB'
 class PrintBook(Book):
     def __init__(self, title, author, page_count: int):
         super().__init__(title, author)
         self.page_count = page_count
 
     def __str__(self):
-        return f'{self.title} by {self.author}, Page Count: {self.page_count}'
+        return f'PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}'
 class Library:
     def __init__(self):
         self.books = []
@@ -28,6 +28,6 @@ class Library:
 
     def list_books(self):
         for book in self.books:
-            print(f'{book.title} by {book.author}')
+            print(book)
 
 
